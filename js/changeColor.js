bcb5f260b0a64e167525c18b459efe6951cb6987
redmoon <danu.ciobanu03@gmail.com>
2020-10-30 02:28:11 +0300

    content, facebook tweeter button

---
+++ js/changeColor.js
@@ -0,0 +1,8 @@
+function chbg(color) {
+    document.getElementById('ico').style.color = color;
+    document.getElementById('ico').style.transition = '0.3s';
+}   
+function chbg2(color) {
+    document.getElementById('ico2').style.color = color;
+    document.getElementById('ico2').style.transition = '0.3s';
+}   
